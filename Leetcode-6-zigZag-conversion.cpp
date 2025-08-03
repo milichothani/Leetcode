f0992b2764848197b82ff96fa41da9db2e6471cf
Mili <milichothani4@gmail.com>
2025-08-03 12:33:22 +0530

    Create Leetcode-6-zigZag-conversion.cpp

---
+++ Leetcode-6-zigZag-conversion.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows==1 || s.length() <= numRows)
+            return s;
+        vector<string> rows(numRows);
+        int row = 0;
+        bool goingDown = false;
+
+        for (char c:s){
+            rows[row] += c;
+            if(row==0 || row == numRows-1)
+                goingDown = !goingDown;
+            row += goingDown? 1: -1;
+        }
+
+        string result;
+        for(string r: rows){
+            result += r;
+        }
+        return result;
+    }
+};
